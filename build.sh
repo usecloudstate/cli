b4724c42acccf57feb9d1b85e3767d3b7d483943
Nadeesha Cabral <n@nadeesha.me>
2021-10-25 21:41:08 +1100

    fix: Build binaries without debug symbols

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ do
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$output_name $package
+    env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-s -w" -o bin/$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
